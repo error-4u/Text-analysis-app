119d9cedb5644053e8d14875aa29ef16fac047ac
singh1475 <vboxuser@ubuntu1.myguest.virtualbox.org>
2023-09-15 11:01:17 +0530

    Streamlit home-page

--- app.py
+++ app.py
@@ -14,3 +14,18 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestClassifier
 
+# Designing side bar
+rad=st.sidebar.radio("Navigation",["Home","Spam or Ham Detection","Sentiment Analysis","Stress Detection","Hate and Offensive Content Detection","Sarcasm Detection"])
+
+# Home page designing
+if rad=="Home":
+    st.title("Complete Text Analysis App")
+    st.image("Complete Text Analysis Home Page.jpg")
+    st.text(" ")
+    st.text("The Following Text Analysis Options Are Available->")
+    st.text(" ")
+    st.text("1. Spam or Ham Detection")
+    st.text("2. Sentiment Analysis")
+    st.text("3. Stress Detection")
+    st.text("4. Hate and Offensive Content Detection")
+    st.text("5. Sarcasm Detection")
